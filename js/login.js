57c29cae6b933b70c45c54cb9066fad1e6c5c867
mursahaul islam noor <mursahaulnur17@gmail.com>
2026-03-07 13:02:12 +0530

    added the signin page and also wore the login function in login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+
+
+document.getElementById("signin-btn").addEventListener('click',()=>{
+    const inputName = document.getElementById("input-username");
+    const name = inputName.value;
+    console.log(name);
+    const inputPass = document.getElementById("input-pass");
+    const pass = inputPass.value;
+    console.log(pass);
+    if(name == "admin" && pass == "admin123"){
+        window.location.assign("/main.html");
+    }else{
+        alert("incorrect credential");
+        return;
+    }
+})
